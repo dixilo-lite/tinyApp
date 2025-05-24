23f556cdf99e32d491809a5066e7bd478dbbefc5
Jasonn Chang <changjason96@gmail.com>
2025-05-23 22:37:10 -0400

    added two url views

--- express_server.js
+++ express_server.js
@@ -19,6 +19,12 @@ app.get("/urls", (req,res) => {
 app.get("/hello",(req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls/:id", (req,res) => {
+  const templateVars = {id: req.params.id,longURL: urlDatabase.id};
+
+  res.render("urls_show",templateVars);
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
